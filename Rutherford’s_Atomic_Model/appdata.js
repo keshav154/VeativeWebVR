087d8f6fcf43b1255f8b0b199e431a95eb2119eb
veative <tech@veative.com>
2020-01-20 17:52:59 +0530

    Enhancement
    Enhancement

--- Rutherford’s_Atomic_Model/appdata.js
+++ Rutherford’s_Atomic_Model/appdata.js
@@ -1,5 +1,5 @@
-app_module_id = "HS400040"
-app_module_name = "Rutherford";
+app_module_id = "HS200040"
+app_module_name = "Rutherford’s Atomic Model";
 app_module_version = "1.0";
 app_device_platform = "";
 
