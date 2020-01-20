1f10af7604f5aac50f28e217d0c86faa5ae94c76
veative <tech@veative.com>
2020-01-20 15:31:17 +0530

    Enhancement
    Enhancement

--- Introduction_to_Complex_Numbers/scripts/appdata.js
+++ Introduction_to_Complex_Numbers/scripts/appdata.js
@@ -4,7 +4,5 @@ app_module_version="1.0";
 app_device_platform="";
 
 // Login profile data
-app_unicef_name="";
-app_unicef_age="";
-app_unicef_avatar="";
-app_unicef_gender="";
+app_unicef_user_id = "";
+app_server_ip="";
